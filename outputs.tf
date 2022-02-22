98e15b2f58d2c15056a232612660a836e71645b6
Constantine Kurianoff <kurianoff@gmail.com>
2022-02-21 21:25:59 -0500

    added lambda role_id to the output

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,12 @@ output "lambda_function" {
     qualified_arn = aws_lambda_function.lambda.qualified_arn
     function_name = aws_lambda_function.lambda.function_name
     role_arn      = aws_iam_role.lambda.arn
+    role_id       = aws_iam_role.lambda.id
   }
   description = "Parameters of created Lambda function: Function Name, ARN, Version, Qualified ARN, IAM role ARN"
 }
 
 output "log_group" {
-  value = aws_cloudwatch_log_group.lambda.arn
+  value       = aws_cloudwatch_log_group.lambda.arn
   description = "ARN of the CloudWatch log group with Lambda execution logs"
 }
